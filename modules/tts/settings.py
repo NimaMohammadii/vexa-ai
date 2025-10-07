fe5f617e3638b9cf0c475963584906b68efbacba
NimaMohammadii <nima.mohammadi084@gmail.com>
2025-10-07 22:52:28 +0330

    Update settings.py

--- modules/tts/settings.py
+++ modules/tts/settings.py
@@ -4,7 +4,7 @@
 STATE_WAIT_TEXT = "tts:wait_text"
 
 # هر کاراکتر = 0.05 کردیت
-CREDIT_PER_CHAR = 0.05
+CREDIT_PER_CHAR = 0.06
 
 # صدای پیش‌فرض (وقتی هنوز چیزی ذخیره نشده)
 DEFAULT_VOICE_NAME = "Nazy"
@@ -16,7 +16,6 @@ VOICES = {
     "Nazy":      "tnSpp4vdxKPjI9w0GnoV",
     "Noushin":   "NZiuR1C6kVMSWHG27sIM",
     "Paniz":       "BZgkqPqms7Kj9ulSkVzn",
-    "Alex":      "m6cSRAItM668kuFQu9OG",
     "Alexandra": "kdmDKE6EkgrWrrykO9Qt",
     "Laura":     "7piC4m7q8WrpEAnMj5xC",
     "Maxon":     "0dPqNXnhg2bmxQv1WKDp",
